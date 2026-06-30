6272fad57c731e1c76a77a64471532be620dd83b
Patrickodida <106867241+Patrickodida@users.noreply.github.com>
2026-06-30 15:41:56 +0300

    DBConnection working - tested successful connection. Add Validator

--- src/main/java/com/hms/util/DBConnection.java
+++ src/main/java/com/hms/util/DBConnection.java
@@ -1,4 +1,33 @@
 package com.hms.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:sqlserver://localhost:1433;"+
+            "databaseName=HospitalDB;"+"encrypt=true;"+"trustServerCertificate=true;";
+
+    private static final String USER = "sa";
+    private static final String PASSWORD = "sap";
+
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try(Connection conn = getConnection()) {
+            System.out.println("SUCCESS: Connected to HospitalDB!");
+        } catch (SQLException e) {
+            System.out.println("FAILED: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/com/hms/util/Validator.java
+++ src/main/java/com/hms/util/Validator.java
@@ -1,4 +1,32 @@
 package com.hms.util;
 
 public class Validator {
+    private Validator() {}
+
+    public static boolean isEmpty(String text) {
+        return text == null || text.isBlank();
+    }
+
+    public static boolean isValidPhone(String phone) {
+        if(isEmpty(phone)) return false;
+        return phone.matches("[+]?[0-9]{10,13}");
+    }
+
+    public static boolean isValidEmail(String email) {
+        if(isEmpty(email)) return false;
+        return email.matches("^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
+    }
+
+    public static boolean isValidName(String name){
+        if(isEmpty(name)) return false;
+        return name.matches("[a-zA-Z '-]{2,50}");
+    }
+
+    public static boolean isPositiveAmount(double amount) {
+        return amount > 0;
+    }
+
+    public static boolean isNonNegativeAmount(int value) {
+        return value >= 0;
+    }
 }
